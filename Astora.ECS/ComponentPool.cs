bec305b57e8cd8132bbe9b1e281ed4a0991643fe
Yuan-Zzzz <1729726526@qq.com>
2025-08-24 00:59:21 +0800

    feat: add World and ComponentPool

---
+++ Astora.ECS/ComponentPool.cs
@@ -0,0 +1,34 @@
+﻿namespace Astora.ECS;
+
+public interface IComponentPool
+{
+    void RemoveIfContains(int entityId);
+}
+
+public sealed class ComponentPool<T> : IComponentPool
+{
+    private readonly SparseSets _set;
+    private readonly T[] _componentInstances;
+
+    public ComponentPool(int maxComponets)
+    {
+        _set = new SparseSets(maxComponets);
+        _componentInstances = new T[maxComponets];
+    }
+
+    public void Add(int entityId, T value)
+    {
+        _set.Add(entityId);
+        _componentInstances[_set.IndexOf(entityId)] = value;
+    }
+
+    public ref T Get(int entityId) => ref _componentInstances[_set.IndexOf(entityId)];
+
+    public bool Contains(int entityId) => _set.Contains(entityId);
+    
+    public void RemoveIfContains(int entityId)
+    {
+        if (Contains(entityId)) Remove(entityId);
+    }
+    private void Remove(int entityId) => _set.Remove(entityId);
+}
---
+++ Astora.ECS/World.cs
@@ -0,0 +1,66 @@
+﻿namespace Astora.ECS;
+using Entity = Int32;
+public class World
+{
+    private readonly int _maxEntities;
+    private Dictionary<Type, IComponentPool> _componentPools = new();
+    private Entity nextEntity = 0;
+    
+    public World(int maxEntities) => _maxEntities = maxEntities;
+
+    /// <summary>
+    /// Ensure that a component pool for type T exists, creating it if necessary.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public ComponentPool<T> Check<T>()
+    {
+        var type = typeof(T);
+        if (_componentPools.TryGetValue(type, out var store)) return (ComponentPool<T>)_componentPools[type];
+        
+        var newStore = new ComponentPool<T>(_maxEntities);
+        _componentPools[type] = newStore;
+        return newStore;
+    } 
+    
+    public Entity Create() => nextEntity++;
+
+    public void Destroy(Entity e)
+    {
+        foreach (var pool in _componentPools.Values)
+        {
+            pool.RemoveIfContains(e);
+        }
+    }
+    
+    public void AddComponent<T>(Entity e, T component)
+    {
+        var store = Check<T>();
+        store.Add(e, component);
+    }
+    
+    public ref T GetComponent<T>(Entity e)
+    {
+        var store = Check<T>();
+        return ref store.Get(e);
+    }
+
+    public bool TryGetComponent<T>(Entity e, ref T component)
+    {
+        var store = Check<T>();
+        if (store.Contains(e))
+        {
+            component = store.Get(e);
+            return true;
+        }
+
+        return false;
+    }
+    
+    public void RemoveComponent<T>(Entity e)
+    {
+        var store = Check<T>();
+        store.RemoveIfContains(e);
+    }
+    
+}
